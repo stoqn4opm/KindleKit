8ae3fc5c47f739c078199d62a3ad97b0651bfd05
Stoyan Stoyanov <stoqn.stoqnov.93@gmail.com>
2018-10-27 20:34:56 +0100

    started KindleKit project

---
+++ KindleKit/KindleKit.h
@@ -0,0 +1,19 @@
+//
+//  KindleKit.h
+//  KindleKit
+//
+//  Created by Stoyan Stoyanov on 27/10/2018.
+//  Copyright © 2018 Stoyan Stoyanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KindleKit.
+FOUNDATION_EXPORT double KindleKitVersionNumber;
+
+//! Project version string for KindleKit.
+FOUNDATION_EXPORT const unsigned char KindleKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KindleKit/PublicHeader.h>
+
+
